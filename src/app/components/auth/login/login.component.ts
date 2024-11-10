a9d4be32b0be9f500c817b98c42f176bf06ced0e
Mike Schauber <mike.schauber96@gmail.com>
2024-11-10 18:30:44 +0100

    create basic login html and css code

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CloudService } from '../../../core/services/cloud.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
