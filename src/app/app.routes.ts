1f85cb26ce8262a74c5f23fe173612961a70c186
Mike Schauber <mike.schauber96@gmail.com>
2024-11-12 19:11:12 +0100

    finish desktop version login for the first moment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ImprintComponent } from './shared/imprint/imprint.component';
+import { PrivacyPolicyComponent } from './shared/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
 ];
--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -2,22 +2,38 @@ import { Component } from '@angular/core';
 import { CloudService } from '../../../core/services/cloud.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterModule
-  
-  ],
+  imports: [CommonModule, RouterModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   constructor(public cloudService: CloudService) {}
-  imageUrl = 'assets/logos/google-icon.svg';
-  showData() {
-    console.log(this.cloudService.channels);
-    console.log(this.cloudService.members);
-    console.log(this.cloudService.ids);
+  mailSvg = 'assets/icons/mail.svg';
+  passwordSvg = 'assets/icons/password.svg';
+  placeholderMail = 'beispielname@email.com';
+  placeholderPw = 'Passwort';
+  focusMailInput() {
+    this.mailSvg = 'assets/icons/mail-bold.svg';
+    this.placeholderMail = '';
+  }
+
+  blurMailInput() {
+    this.mailSvg = 'assets/icons/mail.svg';
+    this.placeholderMail = 'beispielname@email.com';
+  }
+
+  focusPwInput() {
+    this.passwordSvg = 'assets/icons/password-bold.svg';
+    this.placeholderPw = '';
+  }
+
+  blurPwInput() {
+    this.passwordSvg = 'assets/icons/password.svg';
+    this.placeholderPw = 'Passwort';
   }
 }
