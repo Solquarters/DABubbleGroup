a1bae3daa7626320bc3c8f895e268d40b20ec9df
Mike Schauber <mike.schauber96@gmail.com>
2024-11-12 20:01:53 +0100

    put register html and scss into forgot Password component

--- src/app/components/auth/forgot-password/forgot-password.component.ts
+++ src/app/components/auth/forgot-password/forgot-password.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './forgot-password.component.html',
-  styleUrl: './forgot-password.component.scss'
+  styleUrl: './forgot-password.component.scss',
 })
 export class ForgotPasswordComponent {
-
+  constructor(public authService: AuthService) {}
 }
