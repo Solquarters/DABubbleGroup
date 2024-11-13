523ec3709e35d9334ef2b563fcd362c7dba77209
Ameer Mohammed <ameer.mohammed.dev@gmail.com>
2024-11-13 15:53:54 +0100

    display_profile_component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgotPasswort', component: ForgotPasswordComponent },
+  {path: 'profile', component: ProfileComponent}
 ];
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DisplayProfileComponent } from './display-profile/display-profile.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [DisplayProfileComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
