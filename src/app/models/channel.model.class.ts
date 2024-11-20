7002f965cf8a0886a1067cbf6ce41925690fb47e
Solquarters <rom.kabucov@gmail.com>
2024-11-20 17:55:00 +0100

    correcting styling for user and others messages deleted odd and even

--- src/app/models/channel.model.class.ts
+++ src/app/models/channel.model.class.ts
@@ -1,40 +1,41 @@
 export class Channel {
   channelId: string;
-  name: string;
-  createdBy: string;
-  createdAt: Date;
-  updatedAt: Date;
+    name: string;
+    createdBy: string;
+    createdAt: Date;
+    updatedAt: Date;
   description?: string;
   memberIds?: string[];
-
-  constructor(
+  
+    constructor(
     channelId: string,
-    name: string,
-    createdBy: string,
-    createdAt: Date = new Date(),
-    updatedAt: Date = new Date(),
+      name: string,
+      createdBy: string,
+      createdAt: Date = new Date(),
+      updatedAt: Date = new Date(),
     description?: string,
     memberIds?: string[]
-  ) {
+    ) {
     this.channelId = channelId;
-    this.name = name;
-    this.createdBy = createdBy;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
+      this.name = name;
+      this.createdBy = createdBy;
+      this.createdAt = createdAt;
+      this.updatedAt = updatedAt;
     this.description = description;
     this.memberIds = memberIds;
-  }
-
-  // Optional: Methode, um ein Channel-Objekt aus Firestore-Daten zu erstellen
-  static fromFirestoreData(data: any, id: string): Channel {
-    return new Channel(
+    }
+  
+    // Optional: Methode, um ein Channel-Objekt aus Firestore-Daten zu erstellen
+    static fromFirestoreData(data: any, id: string): Channel {
+      return new Channel(
       data.channelId,
-      data.name,
-      data.createdBy,
-      new Date(data.createdAt),
-      new Date(data.updatedAt),
+        data.name,
+        data.createdBy,
+        new Date(data.createdAt),
+        new Date(data.updatedAt),
       data.description,
       data.memberIds?.map((memberId: string) => memberId.toString()) || []
-    );
+      );
+    }
   }
-}
+  
