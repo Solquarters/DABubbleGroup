a40603aeaa5a61886aef9a17e433ff395fd11c14
Solquarters <rom.kabucov@gmail.com>
2024-11-22 11:48:42 +0100

    thread opening functionality inside threadbar component

--- src/app/components/dashboard/thread-bar/thread-bar.component.ts
+++ src/app/components/dashboard/thread-bar/thread-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-thread-bar',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './thread-bar.component.scss'
 })
 export class ThreadBarComponent {
+  @Output() close = new EventEmitter<void>();
 
+  closeThreadBar() {
+    this.close.emit();
+  }
 }
