12d29c2f8ea3f53af25719afdddd892462082628
Solquarters <rom.kabucov@gmail.com>
2024-11-27 18:40:03 +0100

    User.service is in place cleaned up chat.component.ts

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,15 +1,37 @@
 import { Injectable } from '@angular/core';
+import { collectionData, Firestore } from '@angular/fire/firestore';
+import { collection } from 'firebase/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../../models/user.class';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private publicUsersSubject = new BehaviorSubject<User[]>([]);
+  public publicUsers$ = this.publicUsersSubject.asObservable();
 
-  constructor() { 
-
+  
+  constructor(private firestore: Firestore) { 
+  this.loadPublicUserData();
 
   }
 
 
   currentUserId: string = 'user1234';
+
+  private loadPublicUserData() {
+    const publicUserDataCollection = collection(this.firestore, 'publicUserData');
+    const publicUserDataObservable = collectionData(publicUserDataCollection, { idField: 'publicUserId' }) as Observable<User[]>;
+  
+    publicUserDataObservable.subscribe({
+      next: (publicUsers) => {
+        this.publicUsersSubject.next(publicUsers);
+        console.log('Fetched public user data:', publicUsers);
+      },
+      error: (error) => {
+        console.error('Error fetching public user data:', error);
+      }
+    });
+  }
 }
