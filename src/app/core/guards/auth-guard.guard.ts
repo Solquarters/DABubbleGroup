9fe089fd0e594410084f83c91500011e1bf2163b
MikeSchauber <mike.schauber96@gmail.com>
2024-12-07 09:21:04 +0100

    delete User service from auth guard

--- src/app/core/guards/auth-guard.guard.ts
+++ src/app/core/guards/auth-guard.guard.ts
@@ -5,7 +5,6 @@ import { UserService } from '../services/user.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
-  const userService = inject(UserService);
   const router = inject(Router);
 
   if (!authService.isLoggedIn()) {
@@ -13,7 +12,5 @@ export const authGuard: CanActivateFn = (route, state) => {
     router.navigate(['/login'], { replaceUrl: true });
     return false; // Zugriff verweigern
   }
-  authService.loadCurrentUserDataFromLocalStorage();
-  userService.currentUserId = authService.currentUserData.publicUserId;
   return true; // Zugriff gewähren
 };
